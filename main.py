603ae5b706fc3b8c6bbd496d2e14e3f051089b17
Dragonmmq <vhmelinenagg@gmail.com>
2026-04-22 23:07:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 
 # 🔑 НАСТРОЙКИ
-TOKEN = os.environ.get("BOT_TOKEN")
+TOKEN = os.environ.get("RAILWAY_TOKEN")
 ADMINS = [1206582825]  # твой ID
 CHANNEL_ID = -1002168740058  # канал
 PORT = int(os.environ.get("PORT", 10000))
 
 if not TOKEN:
-    raise ValueError("❌ BOT_TOKEN не найден")
+    raise ValueError("❌ RAILWAY_TOKEN не найден")
 
 logging.basicConfig(level=logging.INFO)
 
